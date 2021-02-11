0d78de08703ae0c14c7534d17022236c981a75ac
Ron <43244104+rons4@users.noreply.github.com>
2021-02-11 15:33:45 +0100

    Source System added

--- signl4_host_notify.sh
+++ signl4_host_notify.sh
@@ -5,7 +5,7 @@ SIGNL4_URL=https://connect.signl4.com/webhook/<team-secret>
 
 IFS='%'
 
-SIGNL4_MSG="{ \"Title\": \"Host $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"State\": \"$4\", \"Additional Info\":\"$5\", \"Nagios notification\": \"$6\", \"X-S4-Service\": \"Nagios\" }"
+SIGNL4_MSG="{ \"Title\": \"Host $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"State\": \"$4\", \"Additional Info\":\"$5\", \"Nagios notification\": \"$6\", \"X-S4-SourceSystem\": \"Nagios\" }"
 
 #Send message to SIGNL4
 curl -L -X POST -H "Content-type: application/json" --data "$SIGNL4_MSG" $SIGNL4_URL
--- signl4_service_notify.sh
+++ signl4_service_notify.sh
@@ -5,7 +5,7 @@ SIGNL4_URL=https://connect.signl4.com/webhook/<team-secret>
 
 IFS='%'
 
-SIGNL4_MSG="{ \"Title\": \"Service $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"Service\": \"$4\", \"State\": \"$5\", \"Additional Info\":\"$6\", \"Nagios notification\": \"$7\", \"X-S4-Service\": \"Nagios\"  }"
+SIGNL4_MSG="{ \"Title\": \"Service $1 notification\", \"Host\": \"$2\", \"IP\": \"$3\", \"Service\": \"$4\", \"State\": \"$5\", \"Additional Info\":\"$6\", \"Nagios notification\": \"$7\", \"X-S4-SourceSystem\": \"Nagios\"  }"
 
 #Send message to SIGNL4
 curl -L -X POST -H "Content-type: application/json" --data "$SIGNL4_MSG" $SIGNL4_URL
